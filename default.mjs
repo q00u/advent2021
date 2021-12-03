aaa9adabef98bd9b29c24dd92be3e65be1bb2ebb
Phoenix Gabriel <github@korv.us>
2021-12-02 23:04:08 -0800

    Initial Commit

---
+++ default.mjs
@@ -0,0 +1,34 @@
+const input = ``;
+
+const test = ``;
+
+// SPLIT LINES
+// const lines = test.split('\n');
+const lines = input.split('\n');
+
+// SPLIT CHARS
+// const chars = test.split('');
+// const chars = input.split('');
+
+// PARSE CHARS
+// const parsed = chars.map(char => parseInt(char));
+// PARSE LINES
+const parsed = lines.map(line => parseInt(line));
+
+// Part 1:
+function puzzle1() {
+    let count = 0;
+    return count;
+}
+
+const part1 = puzzle1();
+console.log('Part 1:', part1);
+
+// Part 2:
+function puzzle2() {
+    let count = 0;
+    return count;
+}
+
+const part2 = puzzle2();
+console.log('Part 2:', part2);
